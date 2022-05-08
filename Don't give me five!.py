e6f1431cc6a3ca2d93fae6e3031c529d7cd689f1
xswayzx <71260149+OleksandrDovgaliuk@users.noreply.github.com>
2022-05-08 11:01:22 +0300

    Update Don't give me five!.py

--- Don't give me five!.py
+++ Don't give me five!.py
@@ -1,16 +1,6 @@
 def dont_give_me_five(start,end):
-    print(start)
-    n = 1
-    if start == 4:
-        start += 2
-        n += 0
-        print(start)
-    while start < end:          
-        if start == 5:
-            start += 1  
-            print(start)
-        else:
-            start += 1
+    n = 0
+    for x in range(start, end+1):
+        if '5' not in str(x):
             n += 1
-            print(start)
     return n
